607d5e0afd7276fe3902ffbc753ced7391ea05e7
yemregurses <gursesyunus@hotmail.com>
2024-05-20 09:46:09 +0300

    🩺 add healthcheck for onfhir service in docker-compose

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f common-data-model/docker/docker-compose-onfhir.yml --project-directory ./ -p dt4h-onfhir up -d
+docker compose -f common-data-model/docker/docker-compose-onfhir.yml --project-directory ./ -p dt4h-onfhir up -d --wait
